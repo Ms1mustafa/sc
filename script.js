c640ef33489843a2a0e9f368c71ebbbce57caa71
Mustafa <mustafaalasadi58@gmail.com>
2023-08-11 01:35:35 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+const ItemDescription = document.getElementById("ItemDescription");
+const ItemDescriptionBody = document.getElementById("ItemDescriptionBody");
+
+let Pipe, Clamp, Wood;
+ItemDescription?.addEventListener("change", function () {
+  if (ItemDescription.value == "Pipe 6M" && !Pipe) {
+    let html = `
+      <tr>
+          <td class = 'pipe'>Pipe 6M</td>
+          <td><input class = 'pipe1' type="number" min = "1" name="pipeQty"></td>
+      </tr>
+    `;
+
+    ItemDescriptionBody?.insertAdjacentHTML("afterbegin", html);
+    Pipe = true;
+  }
+  if (ItemDescription.value == "Clamp movable" && !Clamp) {
+    let html = `
+      <tr>
+          <td  class = 'clamp' >Clamp movable</td>
+          <td><input  class = 'clamp1' type="number" min = "1" name="clampQty"></td>
+      </tr>
+    `;
+
+    ItemDescriptionBody.insertAdjacentHTML("afterbegin", html);
+    Clamp = true;
+  }
+  if (ItemDescription.value == "Wood 4m" && !Wood) {
+    let html = `
+      <tr>
+          <td class = 'Wood'>Wood 4m</td>
+          <td><input class = 'Wood1' type="number" min = "1" name="woodQty"></td>
+      </tr>
+    `;
+
+    ItemDescriptionBody.insertAdjacentHTML("afterbegin", html);
+    Wood = true;
+  }
+});
+
 const lengthValue = document.getElementById("length");
 const width = document.getElementById("width");
 const height = document.getElementById("height");
@@ -28,46 +68,6 @@ userType.addEventListener('change', () => {
     areas.style.display = 'block';
   }
 })
-
-const ItemDescription = document.getElementById("ItemDescription");
-const ItemDescriptionBody = document.getElementById("ItemDescriptionBody");
-
-let Pipe, Clamp, Wood;
-ItemDescription?.addEventListener("change", function () {
-  if (ItemDescription.value == "Pipe 6M" && !Pipe) {
-    let html = `
-      <tr>
-          <td class = 'pipe'>Pipe 6M</td>
-          <td><input class = 'pipe1' type="number" min = "1" name="pipeQty"></td>
-      </tr>
-    `;
-
-    ItemDescriptionBody.insertAdjacentHTML("afterbegin", html);
-    Pipe = true;
-  }
-  if (ItemDescription.value == "Clamp movable" && !Clamp) {
-    let html = `
-      <tr>
-          <td  class = 'clamp' >Clamp movable</td>
-          <td><input  class = 'clamp1' type="number" min = "1" name="clampQty"></td>
-      </tr>
-    `;
-
-    ItemDescriptionBody.insertAdjacentHTML("afterbegin", html);
-    Clamp = true;
-  }
-  if (ItemDescription.value == "Wood 4m" && !Wood) {
-    let html = `
-      <tr>
-          <td class = 'Wood'>Wood 4m</td>
-          <td><input class = 'Wood1' type="number" min = "1" name="woodQty"></td>
-      </tr>
-    `;
-
-    ItemDescriptionBody.insertAdjacentHTML("afterbegin", html);
-    Wood = true;
-  }
-});
 
 const reject = document.getElementById('reject');
 const rejectReason = document.getElementById('rejectReason');
