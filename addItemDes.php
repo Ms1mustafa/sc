d6a6d12ce268b1d626752fd444527f9b5bc1ef58
gj-0l <119807853+gj-0l@users.noreply.github.com>
2023-09-01 14:14:43 +0300

    ran

--- addItemDes.php
+++ addItemDes.php
@@ -42,7 +42,7 @@ if (isset($_POST["submit"])) {
         <a class="Back" href="ownerPage.php">
             <i class="fa-solid fa-arrow-left"></i> Back</a>
     </div>
-  <div class="wrappe">
+  <div class="wrappereq">
         <div class="login-container" id="login">
       <div class="top">
 
--- addWorkType.php
+++ addWorkType.php
@@ -35,7 +35,7 @@ if (isset($_POST["submit"])) {
         <a class="Back" href="ownerPage.php">
             <i class="fa-solid fa-arrow-left"></i> Back</a>
     </div>
-<div class="wrappe">
+<div class="wrappereq">
 
   <div class="login-container" id="login">
 <div class="top">
