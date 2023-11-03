785c270a996b8f7445bff4be0a535b281c22f5cf
Mustafa <mustafaalasadi58@gmail.com>
2023-11-03 20:18:58 +0300

    Update

--- includes/config.php
+++ includes/config.php
@@ -4,11 +4,10 @@ ob_start();
 
 date_default_timezone_set('Asia/Baghdad');
 
-try{
-    $con = new PDO("mysql:dbname=sca;host:localhost", "root", "");
+try {
+    $con = new PDO("mysql:dbname=sca;host:localhost", "root", "123456789");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-}
-catch(PDOException $e){
+} catch (PDOException $e) {
     exit("Connection failed: " . $e->getMessage());
 }
 ?>
