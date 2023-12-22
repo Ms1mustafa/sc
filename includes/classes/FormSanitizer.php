f13ad893eff3717779c2ac6d6509456096799ac3
Mustafa <mustafaalasadi58@gmail.com>
2023-12-22 22:08:30 +0300

    update

--- includes/classes/FormSanitizer.php
+++ includes/classes/FormSanitizer.php
@@ -56,5 +56,38 @@ class FormSanitizer
         return date("M Y", $inputTime); // Format as 'month year' (e.g., Jan 2023)
     }
 
+    public static function formatTimeDifference($startDate, $endDate)
+    {
+        $startTimestamp = strtotime($startDate);
+        $endTimestamp = strtotime($endDate);
+
+        if ($startTimestamp === false || $endTimestamp === false) {
+            return "Invalid date format";
+        }
+
+        $timeDiff = $endTimestamp - $startTimestamp;
+        $minute = 60;
+        $hour = 60 * $minute;
+        $day = 24 * $hour;
+
+        if ($timeDiff < $minute) {
+            return "Just now";
+        } elseif ($timeDiff < $hour) {
+            $minutesAgo = floor($timeDiff / $minute);
+            return $minutesAgo . " min ago";
+        } elseif ($timeDiff < 2 * $hour) {
+            return "An hour ago";
+        } elseif ($timeDiff < $day) {
+            $hoursAgo = floor($timeDiff / $hour);
+            return $hoursAgo . " hours ago";
+        } elseif ($timeDiff < 2 * $day) {
+            return "Yesterday";
+        } else {
+            $daysAgo = floor($timeDiff / $day);
+            return $daysAgo . " day" . ($daysAgo > 1 ? 's' : '') . " ago";
+        }
+    }
+
+
 }
 ?>
--- super.php
+++ super.php
@@ -5,10 +5,12 @@ include_once('includes/classes/Request.php');
 include_once('includes/classes/Powers.php');
 include_once('includes/classes/FormSanitizer.php');
 
+$account = new Account($con);
 $request = new Request($con);
 $Area = new Area($con);
 
 $filter = @$_POST["month"] ?? date('Y-m');
+$requests = $request->getRequestDetails(null, null, 'all');
 // echo FormSanitizer::formatMonthYear('2023-01');
 ?>
 <!DOCTYPE html>
@@ -16,7 +18,7 @@ $filter = @$_POST["month"] ?? date('Y-m');
 
 <head>
   <meta charset="UTF-8">
-  
+
   <!-- <link rel="stylesheet" href="../shared.css" /> -->
   <link rel="stylesheet" href="dashboard.css">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -61,7 +63,7 @@ $filter = @$_POST["month"] ?? date('Y-m');
     </table>
   </section>
 
- 
+
   <section>
     <label class="LabelRq" for="">Request the best</label>
     <table class="alluser">
@@ -71,51 +73,122 @@ $filter = @$_POST["month"] ?? date('Y-m');
           <th>Pending In</th>
           <th>Type pending</th>
           <th>Pending time</th>
-      </tr>
-        </thead>
-        <tr>
-        <td>20230001</td>
-        <td>20230001</td>
-        <td>20230001</td>
-        <td>20230001</td>
         </tr>
+      </thead>
+      <tbody>
+        <?php
+        foreach ($requests as $req) {
+          $status = $req["status"];
+          $qtyBackStatus = $req["qtyBackStatus"];
+          $qtyBackDate = $req["qtyBackDate"];
+          $executer = $req["executer"];
+          $wereHouse = $req["wereHouse"];
+          $inspector = $req["inspector"];
+          $reqDate = $req["reqDate"];
+          $executerDate = $req["executerDate"];
+          $wereHouseDate = $req["wereHouseDate"];
+          $inspectorDate = $req["inspectorDate"];
+          $reqType = $req["type_req"];
+
+          $pendingTime = '';
+          if (ucfirst($account->getTypeByName($req["pending_in"])) === "WereHouse") {
+            $pendingTime = FormSanitizer::formatTimeDifference($qtyBackDate ? FormSanitizer::formatDate($qtyBackDate) : $executerDate, date('Y-m-d H:i:s.u'));
+          }
+
+          if (ucfirst($account->getTypeByName($req["pending_in"])) === "Execution") {
+            if ($qtyBackDate) {
+              $pendingTime = FormSanitizer::formatTimeDifference($qtyBackDate, date('Y-m-d H:i:s.u'));
+            } elseif ($inspectorDate) {
+              $pendingTime = FormSanitizer::formatTimeDifference($inspectorDate, date('Y-m-d H:i:s.u'));
+            } elseif ($wereHouseDate) {
+              $pendingTime = FormSanitizer::formatTimeDifference($wereHouseDate, date('Y-m-d H:i:s.u'));
+            }
+          }
+
+          if (ucfirst($account->getTypeByName($req["pending_in"])) === "WereHouse") {
+            $pendingTime = FormSanitizer::formatTimeDifference($qtyBackDate ? FormSanitizer::formatDate($qtyBackDate) : $executerDate, date('Y-m-d H:i:s.u'));
+          }
+
+          if (ucfirst($account->getTypeByName($req["pending_in"])) === "Inspector") {
+            $executerAcceptDate = $data["executerAcceptDate"] ? FormSanitizer::formatDate($data["executerAcceptDate"]) : null;
+            $resentDate = $data["resentDate"] ? FormSanitizer::formatDate($data["resentDate"]) : null;
+            if ($status == 'resentInspector') {
+              $pendingTime = FormSanitizer::formatTimeDifference($resentDate, date('Y-m-d H:i:s.u'));
+            } else {
+              $pendingTime = FormSanitizer::formatTimeDifference($executerAcceptDate, date('Y-m-d H:i:s.u'));
+            }
+          }
+
+          // $pendingTime = FormSanitizer::formatTimeDifference($reqDate, date('Y-m-d H:i:s.u'));
+          // if ($reqType === 'Executer')
+          //   $pendingTime = FormSanitizer::formatTimeDifference($executerDate, date('Y-m-d H:i:s.u'));
         
+          // if ($reqType === 'Reject' && $inspectorDate)
+          //   $pendingTime = FormSanitizer::formatTimeDifference($inspectorDate, date('Y-m-d H:i:s.u'));
+        
+          // if ($reqType === 'Reject' && !$inspectorDate)
+          //   $pendingTime = FormSanitizer::formatTimeDifference($executerDate, date('Y-m-d H:i:s.u'));
+        
+          // if ($reqType === 'WereHouse')
+          //   $pendingTime = FormSanitizer::formatTimeDifference($executerDate, date('Y-m-d H:i:s.u'));
+        
+          // if ($reqType === 'Inspector')
+          //   $pendingTime = FormSanitizer::formatTimeDifference($wereHouseDate, date('Y-m-d H:i:s.u'));
+        
+          echo '
+                    <tr>
+                        <td>' . $req["reqNo"] . '</a></td>
+                        <td>' . $req["pending_in"] . '</td>
+                        <td>' . ucfirst($account->getTypeByName($req["pending_in"])) . '</td>
+                        <td>' . $pendingTime . '</td>
+                        </tr>
+                    ';
+        }
+        ?>
+      </tbody>
+      <!-- <tr>
+        <td>20230001</td>
+        <td>20230001</td>
+        <td>20230001</td>
+        <td>20230001</td>
+      </tr> -->
+
       </tablel>
-      </section>
-  
-    <section class="sectiondas">
-      <label class="" for=""></label>
-      <table class="alluser">
-        <form action="" method="POST">
-          <input type="month" id="month" name="month" value=<?php echo $filter; ?>>
-          <input class="" type="submit" value="filter">
-        </form>
-        <tr>
-          <th>Area</th>
-          <th>Orders</th>
-        </tr>
-        <tbody>
-          <?php
-          foreach ($Area->getArea(true) as $area) {
-            echo "
+  </section>
+
+  <section class="sectiondas">
+    <label class="" for=""></label>
+    <table class="alluser">
+      <form action="" method="POST">
+        <input type="month" id="month" name="month" value=<?php echo $filter; ?>>
+        <input class="" type="submit" value="filter">
+      </form>
+      <tr>
+        <th>Area</th>
+        <th>Orders</th>
+      </tr>
+      <tbody>
+        <?php
+        foreach ($Area->getArea(true) as $area) {
+          echo "
             <tr>
           <td>";
-            echo $area['name'] . "</td>
+          echo $area['name'] . "</td>
           <td>";
-            echo $request->getRequestNum($area['name'], null, $filter) . "
+          echo $request->getRequestNum($area['name'], null, $filter) . "
           </tr>
           ";
-            // echo "<td>" . $area['name'] . "</td>";
-          }
-       
-          ?>
-        </tbody>
-      
-      </table>
-    </section>
+          // echo "<td>" . $area['name'] . "</td>";
+        }
+
+        ?>
+      </tbody>
+
+    </table>
+  </section>
 
 
-  
+
 
 
 </body>
